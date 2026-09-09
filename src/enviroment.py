a7320f7d11de51624a9dc5da1c79a6f5f578b516
sida <hgtfhjii@gmail.com>
2026-09-09 09:31:06 +0700

    env

---
+++ src/enviroment.py
@@ -0,0 +1,19 @@
+
+
+class Environment:
+
+    def __init__(self, width, height, depth):
+        self.width = width
+        self.height = height
+        self.depth = depth
+
+
+    def clip(self,x, y):
+
+        if x<0:
+            x = 0
+        elif x>self.width:
+            x = self.width
+        
+        if y
+
